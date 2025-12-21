2d95be3eebc3516dc52ff60ee999cff076abcf5a
hbuMi <miki2.julin@gmail.com>
2025-12-21 23:52:04 +0200

    Week 7 excercise 3 and 4 done

---
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -0,0 +1,36 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+def main():
+    while True:
+        print("Valitse pelataanko"
+              "\n (a) Ihmistä vastaan"
+              "\n (b) Tekoälyä vastaan"
+              "\n (c) Parannettua tekoälyä vastaan"
+              "\nMuilla valinnoilla lopetetaan"
+              )
+
+        vastaus = input()
+        peli = luo_peli(vastaus)
+
+        if peli:
+            print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
+            peli.pelaa()
+        else:
+            break
+
+
+def luo_peli(tyyppi):
+    if tyyppi == 'a':
+        return KPSPelaajaVsPelaaja()
+    if tyyppi == 'b':
+        return KPSTekoaly()
+    if tyyppi == 'c':
+        return KPSParempiTekoaly()
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
